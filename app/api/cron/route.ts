e7e9f727f2f7f90ca72c8343f05a468179c610e8
Sharaf <sherefbarcelona@gmail.com>
2024-08-17 21:30:06 +0530

    fix deploy error

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -5,8 +5,8 @@ import { getAveragePrice, getEmailNotifType, getHighestPrice, getLowestPrice } f
 import { generateEmailBody, sendEmail } from '@/lib/nodemailer';
 import { NextResponse } from 'next/server';
 
-export const maxDuration = 300; //5 minutes...
-export const dynamic = 'force-dynamic'
+export const maxDuration = 60; //60 seconds...
+export const dynamic = 'force-dynamic';
 export const revalidate = 0;
 
 export async function GET() {
@@ -39,31 +39,30 @@ export async function GET() {
 
         const updatedProduct = await Product.findOneAndUpdate({ url: product.url }, product);
 
-
         //2. CHECK EACH PRODUCT'S STATUS AND SEND EMAIL ACCORDINGLY
-          const emailNotifType = getEmailNotifType(scrapedProduct, currentProduct)
+        const emailNotifType = getEmailNotifType(scrapedProduct, currentProduct);
 
-          if(emailNotifType && updatedProduct.user.length > 0) {
-            const productInfo = {
-              title: updatedProduct.title,
-              url: updatedProduct.url,
-            }
+        if (emailNotifType && updatedProduct.user.length > 0) {
+          const productInfo = {
+            title: updatedProduct.title,
+            url: updatedProduct.url,
+          };
 
-            const emailContent = await generateEmailBody(productInfo, emailNotifType)
+          const emailContent = await generateEmailBody(productInfo, emailNotifType);
 
-            const userEmails = updatedProduct.users.map((user: any) => user.email)
+          const userEmails = updatedProduct.users.map((user: any) => user.email);
 
-            await sendEmail(emailContent, userEmails);
-          } 
+          await sendEmail(emailContent, userEmails);
+        }
 
-
-          return updatedProduct;
+        return updatedProduct;
       })
     );
 
     return NextResponse.json({
-      message: 'Ok', data: updatedProducts,
-    })
+      message: 'Ok',
+      data: updatedProducts,
+    });
   } catch (error) {
     console.error(error);
   }
